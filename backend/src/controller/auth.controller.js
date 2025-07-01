dbd186d94de016899abfce81d048ff711e07b099
prajwalbayari <prajwalbayari4@gmail.com>
2025-07-01 15:51:32 +0530

    Completed auth controller and cloudinary setup

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -3,6 +3,7 @@ import Admin from "../models/admin.model.js";
 import { generateToken } from "../lib/token.js";
 import bcrypt from "bcryptjs";
 
+// Login for the admin
 export const adminLogin = async (req, res) => {
   const { adminEmail, password } = req.body;
   try {
@@ -16,7 +17,7 @@ export const adminLogin = async (req, res) => {
     const checkPassword = await bcrypt.compare(password, admin.password);
 
     if (!checkPassword)
-      return res.status(400).json({ message: "Invalid password!" });
+      return res.status(400).json({ message: "Invalid credentials!" });
 
     generateToken(admin._id, res);
 
@@ -30,5 +31,84 @@ export const adminLogin = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-export const userLogin = async (req, res) => {};
-export const userSignup = async (req, res) => {};
+
+// Signup for the user
+export const userSignup = async (req, res) => {
+  const { userName, userEmail, password } = req.body;
+  try {
+    if (!userEmail || !userName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 8 characters" });
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(userEmail)) {
+      return res.status(200).json({ message: "Invalid email format!" });
+    }
+
+    const user = await User.findOne({ userEmail: userEmail });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      userName,
+      userEmail,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        name: newUser.userName,
+        email: newUser.userEmail,
+        _id: newUser._id,
+        message: "Account created successfully!",
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json("Internal server Error");
+  }
+};
+
+// Login fro the user
+export const userLogin = async (req, res) => {
+  const { userEmail, password } = req.body;
+  try {
+    if (!userEmail || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ userEmail }).select("+password");
+    if (!user) return res.status(400).json({ message: "Invalid credentials!" });
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword)
+      return res.status(400).json({ message: "Invalid credentials!" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      message: "Login successfull",
+      _id: user._id,
+      userEmail,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json("Internal server Error");
+  }
+};
