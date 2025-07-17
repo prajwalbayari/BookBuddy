d2c91e2e452f88d758a307a175a936472d5da052
prajwalbayari <prajwalbayari4@gmail.com>
2025-07-17 14:04:34 +0530

    Updated token.js

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -8,6 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 24 * 60 * 60 * 1000, //Maximum age of cookie in Milliseconds
     httpOnly: true, //Prevents attacks like XSS cross-site scripting attacks
+    sameSite:"none", // Allows cross-site cookie usage
     secure: process.env.NODE_ENV !== "development", //http or https
   });
 
