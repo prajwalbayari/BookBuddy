f0f1a2cbd6ffd7a8fad9f0e1ba10fb72a9ad36a7
prajwalbayari <prajwalbayari4@gmail.com>
2025-07-17 10:47:04 +0530

    Deployment fix 1 (Removed same site)

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -8,7 +8,6 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 24 * 60 * 60 * 1000, //Maximum age of cookie in Milliseconds
     httpOnly: true, //Prevents attacks like XSS cross-site scripting attacks
-    sameSite: "strict", //CSRF attacks cross-ste request forgery attacks
     secure: process.env.NODE_ENV !== "development", //http or https
   });
 
